de7abfcdf5a1e0cee1d6ee1ab0f125ae11083e89
Miglianis Salazar C <migsalazar@deloitte.com>
2025-03-24 13:24:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+import http from 'k6/http';
+import { check } from 'k6';
+ 
+// Obtener variables de entorno
+const API_URL = __ENV.API_URL || 'https://test.k6.io/login.php'; // URL por defecto
+const client_id = __ENV.CLIENT_ID || 'test'; // clientId por defecto
+const client_secret =__ENV.CLIENT_SECRET || '1234'; // clientSecret por defecto
+const scope = __ENV.SCOPE ||'';
+
+
+export default function () {
+    
+  const payload = `grant_type=client_credentials&scope=${scope}&client_id=${client_id}&client_secret=${client_secret}`;
+    
+  const res = http.post(API_URL, payload, {
+      headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+      },
+  });
+
+  check(res, {
+      'token': (r) => r.status === 200,
+      'respuesta tiene token': (r) => JSON.parse(r.body).access_token !== undefined,
+  });
+ 
+  const jsonResponse = JSON.parse(res.body);
+  console.log('Token para cliente:', client_id);
+  console.log('Respuesta: ', jsonResponse.access_token);// Retorna el token
+
+}
